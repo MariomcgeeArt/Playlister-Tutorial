d2a9524a6bbba52fe4bb5cf703319c97d7b9b2a2
Mario McGee <mario.mcgee@students.makeschool.com>
2019-12-03 19:44:12 -0800

    commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 client = MongoClient()
 db = client.Playlister
@@ -7,6 +8,14 @@ playlists = db.playlists
 
 
 app = Flask(__name__)
+
+def video_url_creator(id_lst):
+    videos = []
+    for vid_id in id_lst:
+        # We know that embedded YouTube videos always have this format
+        video = 'https://youtube.com/embed/' + vid_id
+        videos.append(video)
+    return videos
 
 # @app.route('/')
 # def index():
@@ -27,5 +36,36 @@ def playlists_index():
     return render_template('playlists_index.html', playlists=playlists.find())
 
 
+@app.route('/playlists/new')
+def playlists_new():
+    """Create a new playlist."""
+    return render_template('playlists_new.html')
+
+
+# Note the methods parameter that explicitly tells the route that this is a POST
+@app.route('/playlists', methods=['POST'])
+def playlists_submit():
+    """Submit a new playlist."""
+    # Grab the video IDs and make a list out of them
+    video_ids = request.form.get('video_ids').split()
+    # call our helper function to create the list of links
+    videos = video_url_creator(video_ids)
+    playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': videos,
+        'video_ids': video_ids
+    }
+    playlists.insert_one(playlist)
+    return redirect(url_for('playlists_show'))
+
+
+
+@app.route('/playlists/<playlist_id>')
+def playlists_show(playlist_id):
+    """Show a single playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    return render_template('playlists_show.html', playlist=playlist)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
